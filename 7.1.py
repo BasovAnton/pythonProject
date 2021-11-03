e3fd483335707152a2720c18bd4d23985438d884
Администратор <anton.iv90@mail.ru>
2021-11-03 15:58:04 +0300

    7.2

--- 7.1.py
+++ 7.1.py
@@ -1,6 +1,8 @@
 if __name__ == "__main__":
     n = int(input("Введите размер таблицы умножения: "))
-    matrix = [[i*j for j in range(1, n+1)] for i in range(1, n+3)]
+    matrix = [
+        [i*j for j in range(1, n+1)]
+        for i in range(1, n+1)]
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
             ceil = matrix[row][col]
--- 7.2.py
+++ 7.2.py
@@ -1,1 +1,9 @@
 if __name__ == "__main__":
+    N = 64
+    rabbit_paws = 4
+    goose_paws = 2
+    max_rabbit = N//rabbit_paws
+    for i in (range(1, max_rabbit)):
+        rabbit = i
+        goose = int((N - i*rabbit_paws)/goose_paws)
+        print(f'{rabbit:^2}, {goose:^2}')
